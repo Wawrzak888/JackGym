b30c4f00bb4548bcf55ac5911a832ad15cfc0717
Tomasz Wawrzak <wawrzak888@gmail.com>
2026-05-21 19:21:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'orthomatrix-v1';
+const CACHE_NAME = 'orthomatrix-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
